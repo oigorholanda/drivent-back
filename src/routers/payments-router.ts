3045d18dba4ddb3deb980e7c76d26a26b1ea4e76
Igor Holanda <oigorholanda@gmail.com>
2023-04-14 01:57:37 -0300

    feat: payments and tickets routes

---
+++ src/routers/payments-router.ts
@@ -0,0 +1,9 @@
+import { Router} from "express";
+
+const paymentsRouter = Router();
+
+paymentsRouter.get("/payments",)
+paymentsRouter.post("/payments/process",)
+
+
+export { paymentsRouter };
---
+++ src/routers/tickets-router.ts
@@ -0,0 +1,10 @@
+import { Router} from "express";
+
+const ticketsRouter = Router();
+
+ticketsRouter.get('/tickets/types', )
+ticketsRouter.get('/tickets', )
+ticketsRouter.post('/tickets', )
+
+
+export { ticketsRouter };
